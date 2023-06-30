f3da4d8ddf60a95b677dd8350144ff51e7879820
Jefferson-F-Ribeiro <ribeiro.jefferson@escolar.ifrn.edu.br>
2023-06-30 19:30:59 -0300

    Adiciona o swagger

--- backend/core/minhaoficina/views.py
+++ backend/core/minhaoficina/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Oficina, Cliente, Veiculo, Servico, Pedido
+from .serializers import OficinaSerializer, ClienteSerializer, VeiculoSerializer, ServicoSerializer, PedidoSerializer
 
-# Create your views here.
+class OficinaViewSet(viewsets.ModelViewSet):
+    queryset = Oficina.objects.all()
+    serializer_class = OficinaSerializer
+
+class ClienteViewSet(viewsets.ModelViewSet):
+    queryset = Cliente.objects.all()
+    serializer_class = ClienteSerializer
+
+class VeiculoViewSet(viewsets.ModelViewSet):
+    queryset = Veiculo.objects.all()
+    serializer_class = VeiculoSerializer
+
+class ServicoViewSet(viewsets.ModelViewSet):
+    queryset = Servico.objects.all()
+    serializer_class = ServicoSerializer
+
+class PedidoViewSet(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
